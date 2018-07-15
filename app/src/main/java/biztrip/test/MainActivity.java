edda12cfe887c67bdb84581469159e3a69789505
midal2 <midal02@gmail.com>
2018-07-16 00:12:58 +0900

    테이블 레이아웃 대충 기록

---
+++ app/src/main/java/biztrip/test/MainActivity.java
@@ -0,0 +1,20 @@
+package biztrip.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_layout, new MainFragment())
+                    .commit();
+        }
+    }
+}
